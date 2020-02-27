3cd368b6e5e9a78ceb646ac48c539df5c8271a05
Glen Jones <gjones@Glens-MacBook-Pro.local>
2020-02-27 16:16:22 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,30 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king"
-let cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+
+// reprsent the first card flipped
+var cardOne = cards[2];
+
+// push the first card onto teh array
+cardsInPlay.push(cardOne);
+
+// push to console
+console.log("The user flipped: " +cardOne);
+
+// resprsent card two
+let cardTwo = cards[3];
+// push onto array
+cardsInPlay.push(cardTwo);
+// push to console
+console.log("The user flipped: " +cardTwo);
+
+//condtion
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert ("You found a match");
+	}
+	else {
+		alert ("Sorry Try again");
+	}
+}
